f7f361bf07d026e4b57a80614cf13735e3eeda43
Owen Melbourne <owenmelbz@gmail.com>
2020-06-05 21:08:14 +0100

    🚀 Feature - Added Tailwind

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'babel-plugin-tailwind-rn/dist/useTailwind'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="bg-blue-600 p-10 h-full w-full">
+      <Text className="text-white">Open up App.js to start working on your app!</Text>
     </View>
   );
 }
